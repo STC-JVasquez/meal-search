e58534e74f6e2e7cc6fcb4fa9475f3c3a66d8ff2
Juan Vasquez <jvasqu53@stu.southtexascollege.edu>
2024-11-24 22:27:25 -0600

    MealsDetails working

--- src/components/MealItem.vue
+++ src/components/MealItem.vue
@@ -8,7 +8,7 @@ const { meal } = defineProps({
 </script>
 <template>
     <div class="bg-white shadow rounded-xl">
-        <router-link :to="{}">
+        <router-link :to="{name: 'MealDetails', params: {id: meal.idMeal}}">
             <img :src="meal.strMealThumb" class="rounded-t h-48 object-cover w-full"/>
         </router-link>
         <div class="p-3">
